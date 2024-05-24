f8a63a3f29b35f8d14902c40e3bcd7aa76a181c3
ivan.cali <ivan.cali@itsincom.it>
2024-05-24 12:05:16 +0200

    Aggiunta getUtenteById su UtenteResource e fix @Path di utenti

--- src/main/java/it/itsincom/webdev2023/rest/UtenteResource.java
+++ src/main/java/it/itsincom/webdev2023/rest/UtenteResource.java
@@ -1,18 +1,13 @@
 package it.itsincom.webdev2023.rest;
 
-import it.itsincom.webdev2023.persistence.model.Utente;
-import it.itsincom.webdev2023.persistence.repository.UtenteRepository;
 import it.itsincom.webdev2023.rest.model.CreateUtenteResponse;
 import it.itsincom.webdev2023.service.UtenteService;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
-@Path("/utenti")
+@Path("/api/utenti")
 public class UtenteResource {
 
     private final UtenteService utenteService;
@@ -25,6 +20,13 @@ public class UtenteResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<CreateUtenteResponse> getAllUtenti() {
-       return utenteService.getAllUtenti();
+        return utenteService.getAllUtenti();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CreateUtenteResponse getUtenteById(@PathParam("id") int id) {
+        return utenteService.getUtenteById(id);
     }
 }
